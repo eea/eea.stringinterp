dac59cde656d49becc92b1128d8cde024583d949
Alin Voinea <contact@avoinea.com>
2021-05-28 20:21:31 +0300

    [JENKINS] - Fix tests on Py2

--- eea/stringinterp/tests/test_doctests.py
+++ eea/stringinterp/tests/test_doctests.py
@@ -1,13 +1,26 @@
 """ Doc tests
 """
 import doctest
+import re
 import unittest
-from eea.stringinterp.tests.base import FUNCTIONAL_TESTING
+
+import six
 from plone.testing import layered
+
+from eea.stringinterp.tests.base import FUNCTIONAL_TESTING
 
 OPTIONFLAGS = (doctest.REPORT_ONLY_FIRST_FAILURE |
                doctest.ELLIPSIS |
                doctest.NORMALIZE_WHITESPACE)
+
+
+class Py23DocChecker(doctest.OutputChecker):
+    def check_output(self, want, got, optionflags):
+        if six.PY2:
+            got = re.sub("u'(.*?)'", "'\\1'", got)
+            got = re.sub(' encoding="utf-8"', '', got)
+            # want = re.sub("b'(.*?)'", "'\\1'", want)
+        return doctest.OutputChecker.check_output(self, want, got, optionflags)
 
 
 def test_suite():
@@ -19,6 +32,7 @@ def test_suite():
             doctest.DocFileSuite(
                 'adapters/__init__.py',
                 optionflags=OPTIONFLAGS,
+                checker=Py23DocChecker(),
                 package='eea.stringinterp'),
             layer=FUNCTIONAL_TESTING),
     ])
